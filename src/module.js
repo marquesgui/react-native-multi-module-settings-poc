60c9a477aa3a64ca2b6d7e04db4ba422933f09e3
Thiago Zenaro <tzenaro@ciandt.com>
2022-03-29 15:14:11 -0300

    adding broadcast communication

--- src/module.js
+++ src/module.js
@@ -1,4 +1,6 @@
-export default class ModuleSettings {
+// import config from './config';
+
+export default class DemoModuleB {
   prefix() {
     return 'settings';
   }
@@ -9,8 +11,28 @@ export default class ModuleSettings {
         id: 'settings',
         label: 'Settings',
         screen: 'settings.settingsScreen',
-        component: require('./ModuleSettingsScreen').default,
+        component: require('./SettingsScreen').default,
       },
     ];
+  }
+
+  components() {
+    return [
+      {
+        id: 'settings.settingsScreen',
+        generator: () => require('./SettingsScreen').default,
+      },
+    ];
+  }
+
+  registerBroadcasts(register) {
+    const broadcastManager = require('./broadcastManager');
+    const broadcasts = broadcastManager.default;
+    const { POSTS_LIST_REVERSE_LAYOUT } = broadcastManager.BROADCASTS;
+    broadcasts.addBroadcast(POSTS_LIST_REVERSE_LAYOUT, register(POSTS_LIST_REVERSE_LAYOUT, 'notify all modules to reset their data'));
+  }
+
+  methods() {
+    return [];
   }
 }
